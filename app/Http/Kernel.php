6a987292412e69fdad9a25096678865b69120396
Khoa Tran Minh <tranminhkhoa1402@gmail.com>
2016-12-17 15:54:52 +0700

    add middlware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,5 +53,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'api.basic' => \App\Http\Middleware\APIBasicAuth::class,
+        'user.login' => \App\Http\Middleware\UserAPI::class,
     ];
 }
