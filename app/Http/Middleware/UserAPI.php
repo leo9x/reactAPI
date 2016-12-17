fcb8ef3bc7291d5b3d2b3277696e6284add0eea8
Khoa Tran Minh <tranminhkhoa1402@gmail.com>
2016-12-17 15:35:49 +0700

    middleware user api

---
+++ app/Http/Middleware/UserAPI.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+use App\Models\User;
+
+class MerchantAPI
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, $next)
+	{
+		$token = env('AUTH_TOKEN', 'NDOWgOhURXwew5UHFI5suSCK');
+		if($request->header('PHP_AUTH_USER') == $token) {
+			$userToken = $request->header('PHP_AUTH_PW', '');
+			$user = User::where('user_token', $userToken)->first();
+			if ($user == null) {
+				return \Response::json(array('success'=>false,'message'=>'UNAUTHORIZED'));
+			} else {
+				return $next($request);
+			}
+		}
+		else
+			return \Response::json(array('success'=>false,'message'=>'API Pass not valid'));
+	}
+
+}
